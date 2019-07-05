fcdd12e17f78bacebf75c5b5306402d84c35165c
Eri Miyaoka <s1260081@std2dc19.u-aizu.ac.jp>
2019-07-05 11:34:32 +0900

    Won/lost message added

--- Dicegame.c
+++ Dicegame.c
@@ -10,7 +10,12 @@ int main(){
   printf("Die 1: %d\n",d1);
   d2=rand() % 6 + 1;
   printf("Die 2: %d\n",d2);
-  printf("Total value: %d\n",d1+d2);
+  if(d1+d2>7){
+    printf("You won!\n");
+  }
+  else{
+    printf("You lost\n");
+  }
 
   return 0;
 }
